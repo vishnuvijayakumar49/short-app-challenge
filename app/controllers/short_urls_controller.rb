57f7aa8dd92ceb4615ed4145db9be50b153ba5e0
vishnu <vishnu@navyatech.in>
2022-03-02 02:03:40 +0530

    Controller and model methods updated with working code

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -4,12 +4,31 @@ class ShortUrlsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    short_url_ids = ShortUrl.select(:id).order('click_count desc').limit(100)
+    short_urls = short_url_ids.map{|id_obj| id_obj.short_code}
+    render status: 200, json: {urls: short_urls}
   end
 
   def create
   end
 
   def show
+    short_code = params[:id]
+    id = decode(short_code)
+    redirect_to(ShortUrl.find(id).full_url)
+  end
+
+  private
+
+  def create_params
+    params.permit!(:full_url)
+  end
+
+  def decode(short_code)
+    i = 0
+    base = ShortUrl::CHARACTERS.length
+    short_code.each_char { |c| i = i * base + ShortUrl::CHARACTERS.index(c) }
+    i
   end
 
 end
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,18 +1,40 @@
 class ShortUrl < ApplicationRecord
+  require 'open-uri'
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
   validate :validate_full_url
 
-  def short_code
+  def short_code #base62encode
+    return nil if id.nil?
+    p_id = id
+    s = ''
+    base = CHARACTERS.length
+    while p_id > 0
+      s << CHARACTERS[p_id.modulo(base)]
+      p_id /= base
+    end
+    s.reverse
   end
 
   def update_title!
+    begin
+      title = open(full_url).read.scan(/<title>(.*?)<\/title>/).flatten
+      self.update(title: title&.first)
+    rescue OpenURI::HTTPError
+      logger.error "Unable fetch the titlr URL: #{full_url}"
+    end
   end
 
   private
 
   def validate_full_url
+    return false unless full_url =~ /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+    true
+  end
+
+  def logger
+    @logger ||= Rails.logger
   end
 
 end
